f7fc6ea8c9ca87f3a540959c7cca77da357b0cdd
Alexandru <aual@gft.com>
2021-02-22 09:23:52 +0100

    Initial Commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Apostol, Alexandru on 22/02/2021.
+//  Copyright © 2021 Apostol, Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
